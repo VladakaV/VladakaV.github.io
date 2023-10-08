902759cd54d39ccad54031f81f761a9977704cd4
VladakaV <130593681+VladakaV@users.noreply.github.com>
2023-10-08 14:41:41 +0300

    Add files via upload

--- script5.js
+++ script5.js
@@ -1,21 +1,29 @@
 function click1(event) {
-    let prices_and_autos = new Map([["Porshe", 200], ["Toyota", 150], ["Chevrolet", 180], ["Lamborghini", 300], ["Lexus", 250]]);
-    let count_of_product = parseInt(document.getElementsByName("input-field-count")[0].value);
+    let prices_and_autos = new Map([
+        ["Porshe", 200],
+        ["Toyota", 150],
+        ["Chevrolet", 180],
+        ["Lamborghini", 300],
+        ["Lexus", 250]]
+        );
+    let count_of_product = parseInt(document.getElementsByName
+        ("input-field-count")[0].value);
     let chosen_auto = document.getElementById("select-product").value;
     let result_text = document.getElementById("result");
-    for (var pair of prices_and_autos) {
+    for (let pair of prices_and_autos) {
         if (chosen_auto === pair[0]) {
             if (isNaN(pair[1]*count_of_product)) {
                 result_text.innerHTML = "Неверно введено количество"
             }
             else {
-                result_text.innerHTML = "Цена = " + pair[1]*count_of_product + "р"
+                result_text.innerHTML = "Цена = "
+                + pair[1]*count_of_product + "р"
             }
             break
         }
     }
 }
-window.addEventListener('DOMContentLoaded', function (event) {
+window.addEventListener("DOMContentLoaded", function (event) {
     console.log("DOM fully loaded and parsed")
     let button_price = document.getElementById("button-price")
     button_price.addEventListener("click", click1)
