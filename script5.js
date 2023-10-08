324c3bc549337bc1dd1751ea5627f0caef481b6f
VladakaV <130593681+VladakaV@users.noreply.github.com>
2023-10-08 14:22:10 +0300

    Add files via upload

--- script5.js
+++ script5.js
@@ -1,9 +1,9 @@
 function click1(event) {
-    let prices_and_autos = new Map([['Porshe', 200], ['Toyota', 150], ['Chevrolet', 180], ['Lamborghini', 300], ['Lexus', 250]])
-    let count_of_product = parseInt(document.getElementsByName("input-field-count")[0].value)
-    let chosen_auto = document.getElementById("select-product").value
-    let result_text = document.getElementById("result")
-    for (let pair of prices_and_autos) {
+    let prices_and_autos = new Map([["Porshe", 200], ["Toyota", 150], ["Chevrolet", 180], ["Lamborghini", 300], ["Lexus", 250]]);
+    let count_of_product = parseInt(document.getElementsByName("input-field-count")[0].value);
+    let chosen_auto = document.getElementById("select-product").value;
+    let result_text = document.getElementById("result");
+    for (var pair of prices_and_autos) {
         if (chosen_auto === pair[0]) {
             if (isNaN(pair[1]*count_of_product)) {
                 result_text.innerHTML = "Неверно введено количество"
